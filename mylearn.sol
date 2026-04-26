e9205d7218e9a464f743829989172093b86fb7fb
Unknown-Future <1846632060@qq.com>
2026-04-26 22:39:05 +0800

    bytes

--- mylearn.sol
+++ mylearn.sol
@@ -3,5 +3,16 @@
 pragma solidity ^0.8.0;
 
 contract my_byte {
+
+    bytes public x;
     
-}
+    function my_write() public {
+        // x[0] = 'a';
+        // x[1] = 'c';
+        x = "hello";
+    }
+
+    function my_read() public returns(bytes memory) {
+        return x;
+    }
+ }
