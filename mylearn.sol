ad42c4d91d71e8a01c406b539a1c6ffba229012d
Unknown-Future <1846632060@qq.com>
2026-04-28 22:44:04 +0800

    night

--- mylearn.sol
+++ mylearn.sol
@@ -6,14 +6,38 @@ contract myLength {
 
 //    string name = "abcdefg";
 
-    bytes8 color = "black"; 
-    bytes light = ;
+    // bytes4 color = 0x12345678; 
 
-    function my_draw() public view returns(uint) {
-        return color.length;
+    // function my_draw() public view returns(bytes1) {
+    //     return bytes1(color);
+    // }
+    
+    // function my_light() public view returns(bytes8) {
+    //     return bytes8(color);
+    // }
+
+    // function my_temp1() public view returns(uint){
+    //     bytes memory temp1 = new bytes(5);
+    //     // temp1[0] = 0x56;
+    //     return temp1.length;
+
+    // }
+
+    // function my_temp2() public view returns(bytes memory){
+    //     bytes memory temp2 = new bytes(5);
+    //     temp2[1] = 0x56;
+    //     return temp2;
+
+    // }
+
+    bytes temp = new bytes(2);
+    
+    function my_help() public {
+        temp[0] = 0x7a;
+        temp[1] = 0x61;
     }
     
-    function my_light() public view returns(uint) {
-        return light.length;
+    function my_change() public view returns(string memory) {
+        return string(temp);
     }
 }
