81ff9e58a5b552c81cc31eb5e89c6bf5b5ea794c
Unknown-Future <1846632060@qq.com>
2026-04-29 14:07:11 +0800

    2hour

--- mylearn.sol
+++ mylearn.sol
@@ -19,17 +19,21 @@ contract myLength {
     // }
 
     //奇怪的代码!!!!!!!!!!!!
-    // function bytes2_to_string(bytes2 name) public pure returns(string memory){
-    //     bytes memory my_name = new bytes(name.length);
-    //     for (uint i=0; i<name.length; i++) {
-    //         my_name[i] = name[i];
-    //     }
-    //      return string(my_name); 
-    // }
+    function bytes2_to_string(bytes2 name) public pure returns(string memory){
+        bytes memory my_name = new bytes(name.length);
+        for (uint i=0; i<name.length; i++) {
+            my_name[i] = name[i];
+        }
+         return string(my_name); 
+    }
 
     // bytes --> bytes2
-    function bytes2_to_bytes4(bytes2 name) public pure returns(bytes4){
-        bytes4 newname = bytes4(name);
-        return newname;
-    }
+    // function bytes2_to_bytes4(bytes2 name) public pure returns(bytes4){
+    //     bytes4 newname = bytes4(name);
+    //     return newname;
+    // }
+
+    // function buling(bytes4 name) public pure returns(bytes4) {
+    //     return name;
+    // }
 }
