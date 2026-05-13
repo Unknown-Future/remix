c22c2a553cd58f0cd18fe9cd34ed9a469e1337e1
Unknown-Future <1846632060@qq.com>
2026-05-13 23:29:52 +0800

    2329

--- mylearn.sol
+++ mylearn.sol
@@ -1,8 +1,22 @@
-// SPDX-License-Identifier: MIT
-
 pragma solidity ^0.8.0;
+// SPDX-License-Identifier: GPL-3.0
 
-contract MyTest1 {
-    string a = "jafoiema";
-    string b = "feuwa";
+// 我们的第一个合约是一个水龙头！
+contract Faucet {
+
+    // 向任何请求的人发放以太币
+    function withdraw(uint256 _withdrawAmount, address payable _to) public {
+
+        // 限制提取金额
+        require(_withdrawAmount <= 1000000000000);
+
+        // 将金额发送给请求它的地址
+        _to.transfer(_withdrawAmount);
+    }
+
+    // 接收以太币的函数。msg.data 必须为空
+    receive() external payable {}
+
+    // 当 msg.data 不为空时调用的回退函数
+    fallback() external payable {}
 }
